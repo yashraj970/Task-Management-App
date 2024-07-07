4cb572dba7c85a4c818a6d73600bc4ca30a365b9
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-07-07 20:39:59 +0530

    TaskItem component added with style

---
+++ task-management-app/src/components/TaskItem/TaskItem.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteTask, toggleTaskStatus } from "../store/taskSlice";
+import styles from "./TaskItem.module.css";
+
+const TaskItem = ({ task }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <li className={styles.taskItem}>
+      <span className={task.completed ? styles.completed : ""}>
+        {task.title}
+      </span>
+      <div className={styles.actions}>
+        <button
+          onClick={() => dispatch(toggleTaskStatus(task.id))}
+          className={task.completed ? styles.undoButton : styles.completeButton}
+        >
+          {task.completed ? "Undo" : "Complete"}
+        </button>
+        <button
+          onClick={() => dispatch(deleteTask(task.id))}
+          className={styles.deleteButton}
+        >
+          Delete
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default TaskItem;
